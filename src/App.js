8ae6cdc0885ff29124f201046fb2e67f41bc7f67
Carine Todmia <ctodmia@gmail.com>
2023-11-22 15:04:40 -0500

    wworking d3 app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import BarChart from './BarChart';
+import sunshine from './sunshine.json'
+import Select from 'react-select'
+import { useState } from 'react'
+
+const options = [
+  {value: 'JAN', label: 'January'},
+  {value: 'APR', label: 'April'},
+  {value: 'DEC', label: 'December'}
+]
+function bigComputation() {
+  alert("Big computation in JavaScript");
+}
 
 function App() {
+  const [month, setMonth] = useState(options[0])
+  const data = sunshine.map(d => {
+    return {city: d.CITY, sunshine: d[month.value]}
+  })
+  .sort((a, b) => b.sunshine - a.sunshine)
+  .slice(0, 20);
+
+  console.log(data)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi there</h1>
+      <button onClick={bigComputation}>Run Computation</button>
+      <div className="header">
+        <Select defaultValue={month} onChange={setMonth} options={options} />
+      </div>
+      <div className="container">
+        <BarChart data={data} height={600} width={900} />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App"
 
-function bigComputation() {
-  alert("Big computation in JavaScript");
-}
-
-const App = () => {
-  return (
-    <div>
-      <h1>Hi there</h1>
-      <button onClick={bigComputation}>Run Computation</button>
-    </div>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
