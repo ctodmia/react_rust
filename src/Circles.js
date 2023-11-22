1118ec25f6b87746f2c431d51f8ed4bd6f07f60a
Carine Todmia <ctodmia@gmail.com>
2023-11-22 16:03:23 -0500

    added animation transition

--- src/Circles.js
+++ src/Circles.js
@@ -2,9 +2,13 @@ import React, {useRef, useState, useEffect} from 'react'
 import {getData} from './utils'
 import {scaleLinear, select } from 'd3'
 
-const Circles = ({width, height}) => {
+let colors = ['#2176ae', "#57b8ff", "#b66d0d", "#fe6847"]
+
+function Circles ({width, height}) {
     const [data, setData] = useState(getData());
     const svgRef = useRef()
+
+    const handleClick = () => setData(getData())
 
     useEffect(() => {
         const maxRadius = 40
@@ -23,10 +27,12 @@ const Circles = ({width, height}) => {
         select(svgRef.current)
             .selectAll('circle')
             .data(data)
+            .transition()
+            .duration(1000)
             .attr('cx', (d) => xScale(d.x))
             .attr('cy', (d) => yScale(d.y))
             .attr('r', (d) => rScale(d.r))
-            .attr('fill', 'black')
+            .style('fill', (d) => colors[d.color])
     }, [data, width, height])
     console.log(`app`, svgRef.current)
     return (
@@ -35,7 +41,7 @@ const Circles = ({width, height}) => {
                 {data.map(d => <circle fill="#fff"></circle>)}
             </svg>
             <div>
-                <button>Refresh Data</button>
+                <button onClick={handleClick}>Refresh Data</button>
             </div>
         </div>
     )
