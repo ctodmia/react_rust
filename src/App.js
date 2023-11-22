0aaae4d727d85d7794d2ea7c32a2352bfe477844
Carine Todmia <ctodmia@gmail.com>
2023-11-22 17:27:33 -0500

    random circles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Circles from './Circles'
-import { useState } from 'react'
 
 const width = 960
 const height = 500
@@ -8,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hi there</h1>
+      <h1>Random Circles</h1>
       <Circles height={height} width={width} />
     </div>
   );
