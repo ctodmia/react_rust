4b97da4061d4270c9e478a1cc5a2f2c9dfa858e6
Carine Todmia <ctodmia@gmail.com>
2023-11-28 08:34:22 -0500

    added ui theme, sidebar, and navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import './App.css';
-import Circles from './Circles'
 
-const width = 960
-const height = 300
+import { useState } from "react";
+import Circles from './Circles'
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import Navbar  from './screens/global/Navbar';
+import { Routes, Route } from "react-router-dom";
+// import Dashboard from '.screens/dashboard';
+// import Form from './screens/form'
+import Sidebar  from './screens/global/Sidebar';
+
+const width = 960;
+const height = 300;
+
 function App() {
 
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
   return (
-    <div className="App">
-      <h1>Changing Circles</h1>
-      <Circles height={height} width={width} />
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+          <div className="app">
+            <Sidebar isSidebar={isSidebar} />
+            <main className="content">
+              <Navbar setIsSidebar={setIsSidebar}/>
+              <Routes>
+              <Route path="/" element={<Circles height={height} width={width} />} />
+            
+              </Routes>
+            </main>
+          </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
